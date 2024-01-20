4df857e2b47b28331ae8fe003b863ad7d039a871
lucas-nasc <lcsnascimento91@gmail.com>
2024-01-20 00:49:43 -0300

    Adiciona funcionalidade X

--- TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesStringTests.cs
@@ -23,35 +23,33 @@ public class ValidacoesStringTests
     }
 
     [Fact]
-    public void DeveContemAPalavraQualquerNoTexto()
-    {
-        // Arrange
-        var texto = "Esse é um texto qualquer";
-        var textoProcurado = "qualquer";
+    
+public void DeveContemAPalavraQualquerNoTexto()
+{
+    // Arrange
+    var texto = "qualquer";
+    var textoProcurado = "qualquer";
 
-        //TODO: Corrigir a chamada do método "ContemCaractere" da seção Act
-        // Act
-         _validacoes.ContemCaractere(texto, textoProcurado);
+    // Act
+    var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
-        // Assert
-        //TODO: Corrigir o Assert.True com base no retorno da chamada ao método
-        Assert.True(true);
-    }
+    // Assert
+    Assert.True(resultado);
+}
 
     [Fact]
-    public void NaoDeveConterAPalavraTesteNoTexto()
-    {
-        // Arrange
-        var texto = "Esse é um texto qualquer";
-        var textoProcurado = "teste";
+public void NaoDeveConterAPalavraTesteNoTexto()
+{
+    // Arrange
+    var texto = "Esse é um texto qualquer";
+    var textoProcurado = "teste";
 
-        // Act
-        var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
+    // Act
+    var resultado = _validacoes.ContemCaractere(texto, textoProcurado);
 
-        // Assert
-        //TODO: Corrigir o Assert.False com base no retorno da chamada ao método
-        Assert.False(true);
-    }
+    // Assert
+    Assert.False(resultado);
+}
 
     //TODO: Corrigir a anotação [Fact]
     public void TextoDeveTerminarComAPalavraProcurado()
